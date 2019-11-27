4c6d2314429c2e74c022c1bad915a0dc0099a9c0
Cjewett_work <Craig9907@gmail.com>
2019-11-27 12:59:26 -0800

    update python algo document

--- python_practice1.py
+++ python_practice1.py
@@ -7,3 +7,23 @@ def add_one(num):
 
 def square_it(num):
     return num * num
+
+def Quicksort(array):
+    result = array.copy()
+    first = array.shift()
+
+
+def mergesort(array):
+
+    return if len(array) <= 1
+    mid = int(len(array) / 2)
+    left = array[0:mid]
+    right = array[mid:]
+
+    return mergehelper(left, right)
+
+
+
+def mergehelper(left,right):
+
+
